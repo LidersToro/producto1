e7a0222cfa37c6722201df90ad74b7d4c61566ab
Liders Toro <liderz250@gmail.com>
2019-03-09 13:51:07 -0400

    con modelo producto

---
+++ producto1/producto1/Models/DataContexts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace producto1.Models
+{
+    public class DataContexts:DbContext
+    {
+        public DataContexts():base("DefaulConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<producto1.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ producto1/producto1/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace producto1.Models
+{
+    public class Producto
+    {
+        public int productoID { get; set; }
+        public string Nombre { get; set; }
+        public double precio { get; set; }
+        
+    }
+}
